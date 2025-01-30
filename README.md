1c07b22d77cb0e1eed89949af4d7cc35be2785dc
juan <mrjfob@byu.edu>
2025-01-29 22:14:09 -0700

    update documentation

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # ais-ai-it-automation
+
+### TODO
+
+- [ ] Get documentation to interact with Hubspot API and document here, specifically to use the [tickets object](https://developers.hubspot.com/docs/reference/api/crm/objects/tickets).
+- [ ] Learn about using the [actions](https://docs.chainlit.io/concepts/action) feature in Chainlit and assess if that will help us send POST request to Hubspot
+- [ ] Build the interface for chatbot
+- [ ] others...
+
+
+
+### set up
+
+Create a virtual environment that installs the required dependencies
+
+```sh
+conda create --name chainlit-project python=3.0
+conda activate chainlit-project
+pip install -r ./requirements.txt # might be incorrect. Try it and update readme if needed!
+```
+
+check that chainlit was installed correctly by running `chainlit --version`
+
+### initialize project
+
+According to the [Chainlit documentation](https://docs.chainlit.io/integrations/openai), create a `.env` file and store your Openai API key in a variable called `OPENAI_API_KEY`
+
+```sh
+OPENAI_API_KEY=<<the-key>>
+```
+
+Then, run the program with:
+
+```sh
+chainlit run app.py -w
+```
+
--- app.py
+++ app.py
@@ -7,7 +7,11 @@ cl.instrument_openai()
 
 settings = {
     "model": "gpt-3.5-turbo",
-    "temperature": 0,
+    "temperature": 1,
+    "max_completion_tokens": 200,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0
     # ... more settings
 }
 
@@ -16,7 +20,7 @@ async def on_message(message: cl.Message):
     response = await client.chat.completions.create(
         messages=[
             {
-                "content": "You are a helpful bot, you always reply in Spanish",
+                "content": "You are an IT support system that reviews user queries and helps diagnose common issues across various IT domains, including networks, servers, storage, databases, and platforms. You gather enough information to then make informed recommendations to the user. Do not engage in conversation. Go straight to the point.\n\nBase your answers on the best common ITIL practices.\n\nOnce the problem has been identified, recommend a solution to the user step by step. Again, straight to the point. Ask if the user is satisfied with the response.\n\nRepeat the process above once if necessary, unless the user indicates that the issue has been resolved.",
                 "role": "system"
             },
             {
